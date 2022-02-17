dd52573070264b76f8d14419957a4f9236ceb5c5
M-Bischoff <93992306+M-Bischoff@users.noreply.github.com>
2022-02-17 15:23:45 -0500

    Initial commit

---
+++ src/test/java/com/example/pupbuddy/PupBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pupbuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PupBuddyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
