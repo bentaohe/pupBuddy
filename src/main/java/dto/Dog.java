dff59ae73145f1f9aee53d9170f895debb18e9b8
M-Bischoff <93992306+M-Bischoff@users.noreply.github.com>
2022-02-17 15:53:11 -0500

    Added dto object classes

---
+++ src/main/java/dto/Dog.java
@@ -0,0 +1,12 @@
+package dto;
+
+import lombok.Data;
+
+@Data
+public class Dog {
+    private int dogId;
+    private String name;
+    private String breed;
+    private int age;
+    private int houseId;
+}
---
+++ src/main/java/dto/Human.java
@@ -0,0 +1,11 @@
+package dto;
+
+import lombok.Data;
+
+@Data
+public class Human {
+    private int humanId;
+    private String name;
+    private String careRole;
+    private int houseId;
+}
