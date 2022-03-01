3c04d6eacc4ed352432170e4b86016ce5d2e9ac6
novemberluch <50756298+novemberluch@users.noreply.github.com>
2022-03-01 16:08:55 -0500

    Update PupBuddyApplicationTests.java
    Implement tests by Mahesh Gowda

--- src/test/java/com/example/pupbuddy/PupBuddyApplicationTests.java
+++ src/test/java/com/example/pupbuddy/PupBuddyApplicationTests.java
@@ -1,7 +1,12 @@
 package com.example.pupbuddy;
 
+import com.example.pupbuddy.dto.Chore;
+import com.example.pupbuddy.dto.Dog;
+import com.example.pupbuddy.dto.Human;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class PupBuddyApplicationTests {
@@ -43,6 +48,37 @@ class PupBuddyApplicationTests {
     }
 
     private void thenReturnErrorMessageDuplicateTask() {
+    }
+
+    /**
+     * Validate that the DTO properties can be set and retrieved for each class
+     * Thanks to Mahesh Gowda
+     */
+    @Test
+    void verifyChoreProperties() {
+        String choreName =  "TestChore";
+
+        Chore chore = new Chore();
+        chore.setChoreName(choreName);
+        assertEquals(choreName, chore.getChoreName());
+    }
+
+    @Test
+    void verifyDogProperties() {
+        String dogName =  "Testy";
+
+        Dog dog = new Dog();
+        dog.setDogName(dogName);
+        assertEquals(dogName, dog.getDogName());
+    }
+
+    @Test
+    void verifyHumanProperties() {
+        String humanName =  "Test Ing";
+
+        Human human = new Human();
+        human.setHumanName(humanName);
+        assertEquals(humanName, human.getHumanName());
     }
 
 }
